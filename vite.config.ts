0d52125ec9df471e2386ec6c43d4c3e57a05b742
hyeongjun <whow1101@naver.com>
2024-04-10 16:14:20 +0900

    [EnvSetup] chore: alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@Assets": path.resolve(__dirname, "./src/assets"),
+      "@Components": path.resolve(__dirname, "./src/components"),
+      "@Pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
+});
