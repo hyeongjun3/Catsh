2b550d856bc29c6994217ac235a13a46b48bcb78
hyeongjun <whow1101@naver.com>
2024-04-28 04:51:38 +0900

    [Icon] feat: Icon 추가

---
+++ src/components/icons/IconPlay.tsx
@@ -0,0 +1,16 @@
+export default function IconPlay() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="34"
+      height="38"
+      viewBox="0 0 34 38"
+      fill="none"
+    >
+      <path
+        d="M32 15.5359C34.6667 17.0755 34.6667 20.9245 32 22.4641L6.5 37.1865C3.83334 38.7261 0.5 36.8016 0.5 33.7224L0.5 4.27757C0.5 1.19837 3.83333 -0.726134 6.5 0.813467L32 15.5359Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
